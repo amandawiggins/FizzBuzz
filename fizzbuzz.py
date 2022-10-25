a3e6f66bd21476c2aad641b39bd1b8a227cbd7af
amanduh <amanduh@amandawiggins.com>
2022-10-25 12:16:50 -0700

    add fizzbuzz logic

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python
 
+for i in range(99):
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif i % 3 == 0:
+        print("fizz")
+        continue
+    elif i % 5 == 0:
+        print("buzz")
+        continue
+    print(i)
+
