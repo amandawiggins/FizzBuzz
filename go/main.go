870f540739bf785fd06fdcfa2f950bb296206108
amanduh <amanduh@amandawiggins.com>
2022-11-02 14:49:42 -0700

    fizzbuzz in source code Go

---
+++ go/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fizzbuzz()
+}
+func fizzbuzz() {
+	for i := 1; i < 101; i++ {
+		var output string
+		if i%3 == 0 {
+			output = output + "fizz"
+		}
+		if i%5 == 0 {
+			output = output + "buzz"
+		}
+		if output == "" {
+			output = strconv.Itoa(i)
+		}
+		fmt.Println(output)
+	}
+}
