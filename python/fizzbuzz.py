3fd721731e117c684dd51e28d0e9870079241cfc
amanduh <amanduh@amandawiggins.com>
2022-11-08 15:54:05 -0700

    created the python function

--- python/fizzbuzz.py
+++ python/fizzbuzz.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 
-for i in range(1, 101):
-    output = ''
+def main():
+    for i in range(1, 101):
+        output = ''
 
-    if i % 3 == 0:
-        output += 'fizz'
+        if i % 3 == 0:
+            output += 'fizz'
 
-    if i % 5 == 0:
-        output += 'buzz'
+        if i % 5 == 0:
+            output += 'buzz'
 
-    if output == '':
-        output = i
+        if output == '':
+            output = i
 
-    print(output)
+        print(output)
+
+if __name__ == '__main__':
+    main()
